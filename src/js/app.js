5e68329a397c85d3010addf3603581bf3884727f
Sameera Lakshan <shan.sameeralakshan@gmail.com>
2019-05-23 08:26:26 +0530

    Built Responsive Styles
    Built responsive styles and programed menu button behaviour (JavaScript)

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
+var ul = document.querySelector('.responsive-nav');
+var menu = document.querySelector('#hamburger');
+var count = false;
 
+
+// MENU BUTTON BEHAVIOUR
+menu.addEventListener('click', function(){
+	if (count === false) {
+			ul.style.display = 'block';
+
+			setTimeout(function(){
+				ul.style.opacity = '1';
+				ul.style.transform = 'rotateZ(0deg)';
+			}, 100)
+			count = true
+		}else {
+			ul.style.opacity = '0';
+			ul.style.transform = 'rotateZ(90deg)';
+
+			setTimeout(function(){
+				ul.style.display = 'none';
+			}, 500)
+			count = false;
+		}
+})

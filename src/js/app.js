fc5479fc5d45ab60037543413ff93267a6d4d50a
Sameera Lakshan <shan.sameeralakshan@gmail.com>
2019-05-23 21:43:05 +0530

    Finished Project
    Built page animation(star animation), scroll reveal and finished project

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
-var ul = document.querySelector('.responsive-nav');
-var menu = document.querySelector('#hamburger');
+var ul 		= document.querySelector('.responsive-nav');
+var menu 	= document.querySelector('#hamburger');
+var star = document.querySelector('.star');
+var menuOut = document.querySelector('#menu-out');
+var exBtn = document.querySelector('#explore-btn');
 var count = false;
 
 
@@ -7,19 +10,66 @@ var count = false;
 menu.addEventListener('click', function(){
 	if (count === false) {
 			ul.style.display = 'block';
+			menuOut.style.display = 'block'
 
 			setTimeout(function(){
 				ul.style.opacity = '1';
 				ul.style.transform = 'rotateZ(0deg)';
+				menuOut.style.opacity = '0.5'
 			}, 100)
 			count = true
 		}else {
 			ul.style.opacity = '0';
 			ul.style.transform = 'rotateZ(90deg)';
+			menuOut.style.opacity = '0'
 
 			setTimeout(function(){
 				ul.style.display = 'none';
+				menuOut.style.display = 'none'
 			}, 500)
 			count = false;
 		}
-})
+});
+
+// MENU-OUT BACKGROUND CLICK BEHAVIOUR
+menuOut.addEventListener('click', function(){
+	ul.style.opacity = '0';
+	ul.style.transform = 'rotateZ(90deg)';
+	menuOut.style.opacity = '0'
+
+	setTimeout(function(){
+		ul.style.display = 'none';
+		menuOut.style.display = 'none'
+	}, 500)
+	count = false;
+});
+
+
+// SCROLL REVEAL
+window.sr = ScrollReveal();
+
+sr.reveal('h1', {
+	duration: 1000,
+	origin: 'top',
+	distance: '200px'
+});
+
+sr.reveal('p', {
+	duration: 1000,
+	origin: 'left',
+	distance: '200px'
+});
+
+// moon
+sr.reveal('#moon', {
+	duration: 1500,
+	origin: 'top',
+	distance: '200px'
+});
+
+// mountain
+sr.reveal('#mountain', {
+	duration: 1500,
+	origin: 'bottom',
+	distance: '400px'
+});
